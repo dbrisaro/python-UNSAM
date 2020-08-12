134965e18731a34cc6af2faa6b960f4bbee69b32
daniu <dbrisaro@gmail.com>
2020-08-12 11:47:49 -0300

    Corrijo el bonus

--- introduccion_python/bonus_hipoteca.py
+++ introduccion_python/bonus_hipoteca.py
@@ -21,6 +21,9 @@ while saldo > 0:
         saldo = saldo * (1+tasa/12) - pago_mensual
         total_pagado = total_pagado + pago_mensual
     mes = mes + 1
+    if saldo < 0:
+        pago_mensual = pago_mensual-saldo
+        saldo = 0
     print(mes, round(total_pagado,2), round(saldo,2))
 
 print('Total pagado:', round(total_pagado, 1))
